b1a5fef87ea5e907a1456b2030fa9fac2eeef6e5
MohammadMzien2 <mohammadlu281@gmail.com>
2022-12-15 16:38:12 +0100

    defined the variabler

---
+++ quiz.js
@@ -0,0 +1,11 @@
+let arrayOfStudent = [];
+let question = {
+    id: '',
+    image: '',
+    names: [
+        '', '', '', ''
+    ]
+};
+let correctAnswers = 0;
+let selectedNum = 0;
+let numOfQuestions = 0;
