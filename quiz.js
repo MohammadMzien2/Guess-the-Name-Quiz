c09b4d042359fdf9a394441149f669474e0d04bf
MohammadMzien2 <mohammadlu281@gmail.com>
2022-12-15 16:48:20 +0100

    function for selcectnumberOfChocie

--- quiz.js
+++ quiz.js
@@ -9,3 +9,15 @@ let question = {
 let correctAnswers = 0;
 let selectedNum = 0;
 let numOfQuestions = 0;
+
+function selcectnumberOfChocie(num, id){
+    selectedNum = num;
+    document.getElementsByClassName('student')[id].classList.add('selectedButton');
+    for (let i = 0; i < document.getElementsByClassName ('student').length; i++){
+        document.getElementsByClassName('student')[i].disabled = true;
+        if (i != id){
+            document.getElementsByClassName('student')[i].classList.remove('selectedButton');
+        }
+    }
+    loadArray(num);
+}
